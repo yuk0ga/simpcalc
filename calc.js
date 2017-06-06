cc446db620b8cfc5af27e8edb353931abdf5dcd8
yuk0ga <koga@dummy-no-MacBook-Pro.local>
2017-06-06 12:11:23 +0900

    addition comp

--- calc.js
+++ calc.js
@@ -2,5 +2,5 @@ function addition(){
   var x = parseInt(document.getElementById("add1").value);
   var y = parseInt(document.getElementById("add2").value);
   var answer = x + y;
-  alert(answer);
+  document.getElementById("addans").value = answer;
 }
